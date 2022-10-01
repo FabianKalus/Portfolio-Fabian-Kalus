d88d188f522c8b31cd22dea00c10d6c9ce346ab4
FabianKalus <104758870+FabianKalus@users.noreply.github.com>
2022-10-01 17:10:16 +0200

    work on skill component

--- src/assets/js/ball_script.js
+++ src/assets/js/ball_script.js
@@ -2,10 +2,14 @@ window.onload = function() {
 	try {
 	  TagCanvas.Start('myCanvas','tags',{
 		textColour: '#2dfcd8',
-		outlineColour: '#fa2759',
+		outlineColour: '#2dfcd8',
 		reverse: true,
 		depth: 0.8,
-		maxSpeed: 0.05
+		maxSpeed: 0.05,
+		textHeight: 20,
+		weight: true,
+		weightSize: 2.0,
+		
 	  });
 	} catch(e) {
 	  // something went wrong, hide the canvas container
