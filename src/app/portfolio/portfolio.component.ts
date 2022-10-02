b11b4aa2ec5a42e7dc1f5b493e0c83a810864172
FabianKalus <104758870+FabianKalus@users.noreply.github.com>
2022-10-02 14:10:28 +0200

    work on portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
 
+
+export class PortfolioComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  play: string = "play";
+  videoPlay: boolean = false;
+
+  playVideo() {
+    this.videoPlay = true;
+
+    console.log(this.videoPlay)
+  }
+  
+
+
+  pauseVideo(){
+    this.videoPlay = false;
+ 
+    console.log(this.videoPlay);
+  }
+
+ 
 
 }
