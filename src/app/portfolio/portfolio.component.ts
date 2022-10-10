05bfde13d6afa0c40400882ea0cdeafcaf39bb27
FabianKalus <104758870+FabianKalus@users.noreply.github.com>
2022-10-10 12:19:40 +0200

    work at portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -19,6 +19,7 @@ export class PortfolioComponent implements OnInit {
 
   play: string = "play";
   videoPlay: boolean = false;
+  imgNumber: number = 0;
 
   elements: any[] = [
     {"name": "Ring of Fire", "img": "assets/img/portfolio/ringOfFire.png"},
@@ -31,11 +32,14 @@ export class PortfolioComponent implements OnInit {
 
   }
   
+  scaleImg(x) {
+    this.imgNumber = x;
+  }
 
 
   pauseVideo(){
     this.videoPlay = false;
- 
+    this.imgNumber = 0;
   }
 
  
